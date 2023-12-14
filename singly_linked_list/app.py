d65db98df10af6517b3d728d6e95afc04f2e20a5
Sergio <sergioricjr7@gmail.com>
2023-12-14 11:27:18 -0300

    remove function added to the singly_linked_list

--- singly_linked_list/app.py
+++ singly_linked_list/app.py
@@ -1,16 +1,18 @@
 # Lista encadeada simples
 # Different from array, the order in memory is not sequential
 
+
 class Node:
     def __init__(self, data) -> None:
         self.data = data
         self.next = None
-    
+
     def set_next():
         pass
 
     def __repr__(self) -> str:
         return self.data
+
 
 class LinkedList:
     def __init__(self) -> None:
@@ -22,7 +24,7 @@ class LinkedList:
         if self.head == None:
             self.head = new_node
             return
-        
+
         current_node = self.head
         while current_node.next:
             current_node = current_node.next
@@ -32,14 +34,14 @@ class LinkedList:
     def length(self):
         if self.head == None:
             return 0
-        
+
         current_node = self.head
         total = 0
         while current_node:
             current_node = current_node.next
             total += 1
         return total
-    
+
     def reverse(self):
         previous_node = None
         current_node = self.head
@@ -49,23 +51,58 @@ class LinkedList:
             previous_node = current_node
             current_node = next
         self.head = previous_node
+
+    def get(self, index):
+        self.check_index(index)
+
+        current_index = 0
+        current_node = self.head
+        while current_node != None:
+            if current_index == index:
+                return current_node.data
+            current_node = current_node.next
+            current_index += 1
+
+    def remove(self, index):
+        self.check_index(index)
+        current_index = 0
+        prev_node = None
+        current_node = self.head
+        while current_node != None:
+            if current_index == index:
+                if prev_node == None:
+                    self.head = current_node.next
+                    current_node.next = None
+                    return
+                prev_node.next = current_node.next
+                current_node.next = None
+                return
+            prev_node = current_node
+            current_node = current_node.next
+            current_index += 1
+            
     
+    def check_index(self, index):
+        if index >= self.length() or index < 0:
+            raise IndexError("invalid index")
+
     def __repr__(self) -> str:
         if self.head == None:
-            return '[]'
-        items = ''
+            return "[]"
+        items = ""
         current_node = self.head
         while current_node.next != None:
-            items += f'{current_node.data}, '
+            items += f"{current_node.data}, "
             current_node = current_node.next
-        items += f' {current_node.data}'
-              
-        return '[' + items + ']'            
+        items += f" {current_node.data}"
 
-node1 = 'sergio'
-node2 = 'ricardo'
-node3 = 'césar'
-node4 = 'Augusto'
+        return "[" + items + "]"
+
+
+node1 = "sergio"
+node2 = "ricardo"
+node3 = "césar"
+node4 = "Augusto"
 
 lista = LinkedList()
 
@@ -74,4 +111,5 @@ lista.append(node2)
 lista.append(node3)
 lista.append(node4)
 lista.reverse()
+lista.remove(0)
 print(lista)
